9d56e0c5f5a5e430027d1a732db8073944418029
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-03-12 17:29:54 +0000

    generated products using js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,57 +1,7 @@
-//  const amazonProducts = [
-//     {
-//         image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-//         name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-//         rating: {
-//             stars: 4.5,
-//             count: 87
-//         },
-//         price: 10.90
-//     },
-
-//     {
-//         image: "images/products/intermediate-composite-basketball.jpg",
-//         name: "Intermediate Size Basketball",
-//         rating: {
-//             stars: 4,
-//             count: 127
-//         },
-//         price: 20.95
-//     },
-
-
-//     {
-//         image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-//         name: "Adults Plain Cotton T-Shirt - 2 Pack",
-//         rating: {
-//             stars: 4.5,
-//             count: 56
-//         },
-//         price: 7.99
-//     },
-// ];
-
-// function formatCurrency(amount){
-//   return Intl.NumberFormat('en-GB', {
-//     style: 'currency',
-//     currency: 'GBP',
-// })
-// .format(amount);
-  
-// }
 import { formatCurrency} from './money.js'
 import { getProducts} from './products.js'
 
-// async function getProducts() {
-//   try {
-//     const response = await axios.get('products.json');
-//     return response.data;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-getProducts().then(products => {
+function generateProductHtml(products){
   let productHtml = '';
 
   products['products'].forEach(product => {
@@ -106,10 +56,12 @@ getProducts().then(products => {
         </div>`;
 
   });
-  console.log(productHtml)
-  // console.log(formatCurrency(12))
 
+  return productHtml;
 
+}
+getProducts().then(products => {
+  document.querySelector('.js-products-grid').innerHTML = generateProductHtml(products);
 
 })
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,41 @@
-async function getProducts() {
-  try {
-    const response = await axios.get('products.json');
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
-}
-// import {getProducts} from './products'
-getProducts().then(products => {
-  console.log('products', products)
+//  const amazonProducts = [
+//     {
+//         image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+//         name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+//         rating: {
+//             stars: 4.5,
+//             count: 87
+//         },
+//         price: 10.90
+//     },
 
-})
+//     {
+//         image: "images/products/intermediate-composite-basketball.jpg",
+//         name: "Intermediate Size Basketball",
+//         rating: {
+//             stars: 4,
+//             count: 127
+//         },
+//         price: 20.95
+//     },
+
+
+//     {
+//         image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+//         name: "Adults Plain Cotton T-Shirt - 2 Pack",
+//         rating: {
+//             stars: 4.5,
+//             count: 56
+//         },
+//         price: 7.99
+//     },
+// ];
+
+// function formatCurrency(amount){
+//   return Intl.NumberFormat('en-GB', {
+//     style: 'currency',
+//     currency: 'GBP',
+// })
+// .format(amount);
+  
+// }
