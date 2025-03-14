937b0664d0f82b19a41c7d6c5ff0d48898a2ac0a
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-03-14 17:27:54 +0000

    updated

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,5 +1,5 @@
 import { getProducts, getProduct } from './../products.js'
-import { cart, removeFromCart, updateDeliveryOption } from "../../data/cart.js";
+import { cart } from "../../data/cart.js";
 import { formatCurrency } from '../utils/money.js';
 
 export const loadPaymentSummary = () => getProducts().then(products => renderPaymentSummary(products));
@@ -8,30 +8,16 @@ function renderPaymentSummary(products) {
     const productPrices = getProduct(cart, products).map(product => product.price);
     const cartSummary = getCartSummary(productPrices)
     const total = cartSummary.reduce((acc, item) => acc + (item.quantity * item.price), 0);
-    console.log(formatCurrency(total))
-
+    const totalFormatted = formatCurrency(total);
+    
 
 }
 function getCartSummary(productPrices) {
-
-    // let cartSummary = [];
-    // cart.forEach((cartItem, i) => {
-        // cartSummary.push({
-        //     price: productPrices[i],
-        //     quantity: cartItem.quantity
-
-        // })
-    // });
-    // return cartSummary
-
     return cart.map((cartItem, i) =>{
         return {
             price: productPrices[i],
             quantity: cartItem.quantity
-
         }
-
-        
     })
     
 }
